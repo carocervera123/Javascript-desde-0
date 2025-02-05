f6e5b0f130bca9e0479701a9076954294beee71e
Carolina Cervera Torres <caro.cer.tor123@gmail.com>
2025-02-04 20:45:24 -0600

    TEMA: tipos de datos

---
+++ tipos-de-dato.js
@@ -0,0 +1,58 @@
+//este es un comentarios en java script
+
+//console.log es un comando que se usa para imprimir en la consola
+
+console.log("Hola a todos");
+
+
+//declaracion de variables
+
+var nombre ="Ana"; //string
+var edad = 23; //Number
+var casada = false; //Boolean
+var vacunas = true; //Boolean
+var hijos = null; //Null
+var dato; //Undefined
+
+//typeof se utiliza para saber el tipo de dato de una variable
+console.log(typeof(nombre));
+console.log(typeof(edad));
+console.log(typeof(sueldo));
+console.log(typeof(casada));
+console.log(typeof(vacunas));
+console.log(typeof(hijos));
+console.log(typeof(dato));
+
+//la semantica es importante, significado de las palabras
+var nombre = "Francia";
+var pais = 20;
+
+//NoN (Not a Number) es un valor especial que indica que una operacion matematica no es un numero
+
+
+console.log(10 * "Ana");
+/* si ponemos typeof de null dira que es un objeto, pero en realidad es ausencia intencional de valor. Esto debido a un error en JavaScript que no se ha corregido por compatibilidad de versiones anteriores.
+
+comentarios en bloque o en linea
+*/
+
+//ejercicios de CAMPUS
+
+console.log(typeof(42));
+console.log(typeof('Veinticinco'));
+console.log(typeof(-666));
+console.log(typeof(true));
+console.log(typeof(0));
+console.log(typeof('"'));
+console.log(typeof(null));
+console.log(typeof(undefined));
+console.log(typeof(false));
+
+//ejercicios extra
+
+var pais= "Mexico";
+console.log(typeof(14));
+console.log(typeof(23*2));
+console.log(typeof(0.23));
+console.log(typeof(pais));
+console.log(typeof('---'));
