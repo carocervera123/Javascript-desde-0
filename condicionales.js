5df212cedf008b5ae36bbc2fa1a6683add176399
Carolina Cervera Torres <caro.cer.tor123@gmail.com>
2025-02-07 14:41:12 -0600

    Clase 2

---
+++ condicionales.js
@@ -0,0 +1,70 @@
+// Tipo de mascotas = Gato, Perro, Hamster
+
+// Condicionales (Jaula mediana, Jaula grande, Jaula peque;a)
+
+let tipodemascota = "Perro";
+
+if (tipodemascota === "Perro"){
+    console.log("si tienes un perro, necesitas la jaula grande.")
+} else if (tipodemascota === "Gato"){
+    console.log("si tienes un gato, necesitas la jaula peque;a")
+} else { //ELSE es el bloque falso (se ejecuta si no se cumple ninguna de las condiciones anteriores)
+    console.log("no contamos con jaulas para esa mascota.")
+}
+
+//semaforo color = Rojo, Amarillo, Verde
+// Rojo= Alto, Amarillo= Detente, Verde=Siga
+
+let color = "Rojo";
+
+if (color === "Rojo"){
+    console.log("Alto")
+}
+else if(color === "Amarillo"){
+    console.log("Detente")
+}
+else if(color === "Verde"){
+    console.log("Pase")
+}
+else {
+    console.log("El color no existe")
+}
+
+
+
+
+let edad = 17;
+let tieneLicencia = true;
+
+if (edad >= 18 && tieneLicencia) {
+    console.log("Puedes conducir");
+} else {
+    console.log("No puedes conducir");
+}
+
+// Para poder obtener una beca
+
+let creditos = 10; // Minimo 10 creditos
+let promedio = 7.5; // Minimo 8.5
+
+if(creditos >= 10 && promedio >= 8.5){
+    console.log("Tienes derecho a una beca");
+}else{
+    console.log("No tienes derecho a una beca");
+}
+
+// Agregar el mensaje de - Te hacen falta creditos 
+// Agregar el mensaje de - Tu promedio debe subir
+let nota = 30.0; // Puedes cambiar este valor para probar diferentes casos
+
+if (nota === "") {
+    console.log("No pusiste la nota");
+} else if (nota >= 90) {
+    console.log("Excelente");
+} else if (nota >= 75) {
+    console.log("Bien");
+} else if (nota >= 60) {
+    console.log("Suficiente");
+} else {
+    console.log("No apruebas");
+}
---
+++ operadores-basicos.js
@@ -0,0 +1,80 @@
+//declaracion de variables
+
+let valor_1 = 27;
+let valor_2 = 4;
+
+//operaciones basicas
+let suma = valor_1 + valor_2;
+console.log(suma);
+
+let resta = valor_1 - valor_2;
+console.log(resta);
+
+let multiplicacion = valor_1 * valor_2;
+console.log(multiplicacion);
+
+let division = valor_1/valor_1;
+console.log(division);
+
+let modulo = valor_1 % valor_2;
+console.log(modulo);
+
+let exponencial = valor_1 ** valor_2;
+console.log(exponencial);
+
+//operadores de comparacion
+
+let numero_1 = 3; //numero
+let numero_2 = "3"; //string
+
+console.log(numero_1 == numero_2); //comparacion debil
+
+console.log(numero_1 ===numero_2); //comparacio fuerte
+
+console.log("operadores logicos y de comparacion");
+
+console.log(10 > 4); //mayor que
+console.log(2 < 1); //menor que
+console.log(10 >= 10); //mayor o igual que 
+console.log(10 <= 10); //menor o igual que
+
+//la respuesta es un valor boleano
+
+console.log("diferente de:");
+console.log(15 != 15); //false porque es el mismo
+
+
+console.log("AND: &&");
+
+let edad = 20;
+
+let tienelicencia = true;
+
+console.log(edad >= 18 && tienelicencia);
+
+
+console.log("OR: ||");
+
+let esMayorDeEdad = false;
+
+let esEstudiante = true;
+
+console.log(esMayorDeEdad || esEstudiante); // true, porque una condición es verdadera
+
+console.log("NOT: !");
+
+let esFinDeSemana = true;
+
+console.log(!esFinDeSemana); // false, porque invierte el valor
+
+
+let calificacion_1 = 9;
+let calificacion_2 = 10;
+let calificacion_3 = 3;
+let calificacion_4 = 7;
+let calificacion_5 = 8;
+
+// += nos permite hacer una suma acumulativa promedio = promedio + calificacion
+
+//+=Nos permite hacer una suma acumulativa
+
